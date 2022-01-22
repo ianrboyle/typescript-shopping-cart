269d697e7ee87ff46ea9ddccb3461dabe115627f
Ian Boyle <irboyle21@gmail.com>
2022-01-22 13:20:06 -0500

    adds requirements for CartItem component

--- src/CartItem/CartItem.tsx
+++ src/CartItem/CartItem.tsx
@@ -3,6 +3,7 @@ import Button from '@material-ui/core/Button'
 
 //Types
 import { CartItemType } from '../App'
+import Item from '../Item/item'
 
 //styles
 import { Wrapper } from './CartItem.styles'
@@ -13,6 +14,23 @@ type Props = {
   removeFromCart: (id: number) => void;
 }
 
-const CartItem: React.FC<Props> = () => <div>Cart Item</div>
+const CartItem: React.FC<Props> = ({item, addToCart, removeFromCart}) => (
+  <Wrapper>
+    <div>
+      <h3>
+        {item.title}
+      </h3>
+      <div className="cart-item-information">
+        <p>Price: ${item.price}</p>
+        <p>Total: ${(item.price * item.quantity).toFixed(2)}</p>
+      </div>
+      <div className="cart-buttons">
+        <Button size="small" disableElevation variant="contained" onClick={() => removeFromCart(item.id)}>-</Button>
+        <p>{item.quantity}</p>
+        <Button size="small" disableElevation variant="contained" onClick={() => addToCart(item)}>+</Button>
+      </div>
+      <img src={item.image} alt={item.title}/>
+    </div>
+  </Wrapper>)
 
 export default CartItem;
