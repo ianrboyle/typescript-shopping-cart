245cf94ec63e69d99888b61804ce5397c91c5b8c
Ian Boyle <irboyle21@gmail.com>
2022-01-22 12:19:41 -0500

    adds stylebutton, cart item badge

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import Badge from '@material-ui/core/Badge'
 import Item from './Item/item'
 
 //Styles
-import {Wrapper} from './App.styles'
+import {Wrapper, StyledButton} from './App.styles'
 
 
 //Types
@@ -37,7 +37,7 @@ const App = () => {
   const {data, isLoading, error} = useQuery<CartItemType[]>('products', getProducts)
     console.log(data)
 
-  const getTotalItems = () => null
+  const getTotalItems = (items: CartItemType[]) => null
 
   const handleAddToCart = (clickedItem: CartItemType) => null
 
@@ -47,6 +47,14 @@ const App = () => {
   if (error) return <div>Something went Wrong...</div>
   return (
     <Wrapper>
+      <Drawer anchor='right' open={cartOpen} onClose={() => setCartOpen(false)}>
+        Cart Goes Here
+      </Drawer>
+      <StyledButton onClick={() => setCartOpen(true)}>
+        <Badge badgeContent={getTotalItems(cartItems)} color='error'>
+          <AddShoppingCartIcon />
+        </Badge>
+      </StyledButton>
       <Grid container spacing={3}>
         {data?.map(item => (
           <Grid item key={item.id} xs={12} sm={4}> 
