627f4828e3efbb8d93de61403ba889f7a774901e
Ian Boyle <irboyle21@gmail.com>
2022-01-22 10:20:45 -0500

    creates getProducts function - fetches products from api. CartItemType created

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,21 @@ import Badge from '@material-ui/core/Badge'
 import {Wrapper} from './App.styles'
 
 
+//Types
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  quantity: number;
+}
+
+
+const getProducts = async (): Promise<CartItemType> => 
+  await (await fetch('https://fakestoreapi.com/products')).json();
+
 const App = () => {
   return (
     <div className="App">
