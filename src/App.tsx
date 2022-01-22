614010440ea8c339b5011e013f9b6bc7fbb8e8db
Ian Boyle <irboyle21@gmail.com>
2022-01-22 10:26:45 -0500

    adds isLoading condition, sets up functions for future use

--- src/App.tsx
+++ src/App.tsx
@@ -31,8 +31,17 @@ const getProducts = async (): Promise<CartItemType[]> =>
   await (await fetch('https://fakestoreapi.com/products')).json();
 
 const App = () => {
-const {data, isLoading, error} = useQuery<CartItemType[]>('products', getProducts)
-  console.log(data)
+  const {data, isLoading, error} = useQuery<CartItemType[]>('products', getProducts)
+    console.log(data)
+
+  const getTotalItems = () => null
+
+  const handleAddToCart = () => null
+
+  const handleRemoveFromCart = () => null
+
+  if (isLoading) return <LinearProgress/>
+  if (error) return <div>Something went Wrong...</div>
   return (
     <div className="App">
       Start
